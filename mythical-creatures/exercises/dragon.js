a7b0a79aef1f652904e0fabf3cc21ccdfee6a5db
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2021-12-06 21:32:35 -0600

    Add Dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
 
+
+class Dragon {
+  constructor(name,rider){
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.stomach = 0
+  }
+greet(rider) {
+  return `Hi, ${this.rider}!`
+  }
+  eat(){
+    this.stomach++
+    if(this.stomach > 2)
+    return this.hungry = false
+    }
+  }
+
+
+module.exports = Dragon;
